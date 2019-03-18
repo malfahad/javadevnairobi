61e146b72bf653b786786ee5bc5f4c5d694e2c37
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-18 16:55:14 +0300

    ft(RecyclerView): connect RecyclerView to main activity
    - Added RecyclerView adapter
    - Added demo data to adapter
    - hooked up RecyclerView and Adapter to MainActivity
    
    [FINISHES #155768857]

--- app/src/main/java/com/andela/javadevsnairobi/MainActivity.java
+++ app/src/main/java/com/andela/javadevsnairobi/MainActivity.java
@@ -1,13 +1,20 @@
 package com.andela.javadevsnairobi;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.andela.javadevsnairobi.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView devsRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        devsRecyclerView = findViewById(R.id.devs_recycler_view);
+        devsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
