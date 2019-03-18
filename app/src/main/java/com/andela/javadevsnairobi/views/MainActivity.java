5e3c4bc0adef07aeffaf084a659d6a61feb862d0
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-18 16:55:14 +0300

    ft(adapter): setup adapter
    - Adds Github adapter
    - Connects Adapter to recyclerView
    - Consume API data
    
    [FINSISHES #155965773]

--- app/src/main/java/com/andela/javadevsnairobi/views/MainActivity.java
+++ app/src/main/java/com/andela/javadevsnairobi/views/MainActivity.java
@@ -1,16 +1,13 @@
 package com.andela.javadevsnairobi.views;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import com.andela.javadevsnairobi.R;
+import com.andela.javadevsnairobi.adapter.GithubAdapter;
 import com.andela.javadevsnairobi.model.GithubUser;
 import com.andela.javadevsnairobi.presenter.GithubPresenter;
 
@@ -19,7 +16,6 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements GithubAllUsersView {
 
     RecyclerView devsRecyclerView;
-    Button launchBtn;
     GithubPresenter presenter;
 
 
@@ -30,21 +26,13 @@ public class MainActivity extends AppCompatActivity implements GithubAllUsersVie
 
         devsRecyclerView = findViewById(R.id.devs_recycler_view);
         devsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        launchBtn = findViewById(R.id.launch_btn);
-        launchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), DevDetailActivity.class);
-                startActivity(intent);
-            }
-        });
         presenter = new GithubPresenter(this);
         presenter.getAllUsers();
     }
 
     @Override
     public void showAllUsers(List<GithubUser> githubUsers) {
-        Log.d("Users Count", String.valueOf(githubUsers.size()));
+        GithubAdapter adapter = new GithubAdapter(githubUsers);
+        devsRecyclerView.setAdapter(adapter);
     }
-
 }
