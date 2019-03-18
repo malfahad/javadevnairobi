9fa2cd4175a071e998ba4141b0e446a2a9ebc4f7
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-18 16:55:14 +0300

    ft(retrofit):  api endpoints
    - defines fetch user endppoint
    - defines fetch all users endpoint
    
    [FINISHES #155903085]

--- app/src/main/java/com/andela/javadevsnairobi/service/GithubAPI.java
+++ app/src/main/java/com/andela/javadevsnairobi/service/GithubAPI.java
@@ -1,5 +1,17 @@
 package com.andela.javadevsnairobi.service;
 
+import com.andela.javadevsnairobi.model.GithubUser;
+import com.andela.javadevsnairobi.model.GithubUsersResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GithubAPI {
 
+    @GET("/users/{username}")
+    Call<GithubUser> getUser(@Path("username") String username);
+
+    @GET("/search/users?q=location:Nairobi+language:Java")
+    Call<GithubUsersResponse> getAllUsers();
 }
