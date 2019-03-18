44933214f811a2ec9fece0ce2297c9a561625858
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-18 16:55:14 +0300

    ft(api): access api
    - adds a github presenter
    - adds view interfaces
    - adds internet permission to manifest
    - consumes api data in detail activity
    
    [FINISHES #155931587]

---
+++ app/src/main/java/com/andela/javadevsnairobi/views/GithubAllUsersView.java
@@ -0,0 +1,11 @@
+package com.andela.javadevsnairobi.views;
+
+import com.andela.javadevsnairobi.model.GithubUser;
+
+import java.util.List;
+
+public interface GithubAllUsersView {
+
+    void showAllUsers(List<GithubUser> githubUsers);
+
+}
