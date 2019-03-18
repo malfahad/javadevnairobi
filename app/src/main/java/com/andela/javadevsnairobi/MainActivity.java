ffcbc533e3776658c7a92ecef35b95585d507d97
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-18 16:55:14 +0300

    ft(detailActivity) launch detaiActivity from mainActivity
    - Add Intent to itemView
    - add back button navigation to detail activity
    
    [FINISHES #155820624]

--- app/src/main/java/com/andela/javadevsnairobi/MainActivity.java
+++ app/src/main/java/com/andela/javadevsnairobi/MainActivity.java
@@ -1,20 +1,35 @@
 package com.andela.javadevsnairobi;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import com.andela.javadevsnairobi.R;
+
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView devsRecyclerView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Button launchBtn;
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         devsRecyclerView = findViewById(R.id.devs_recycler_view);
         devsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
+        launchBtn = findViewById(R.id.launch_btn);
+        launchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), DevDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+  }
 
 }
