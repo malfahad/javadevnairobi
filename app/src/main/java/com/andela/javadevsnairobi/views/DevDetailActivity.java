3c5061450270728a4389f273f87ba5993ef3ab0f
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-03-28 12:31:41 +0300

    ft(share): share users profile
    - create share intent
    - launch inent chooser for intent
    
    [FINISHES #156008359]

--- app/src/main/java/com/andela/javadevsnairobi/views/DevDetailActivity.java
+++ app/src/main/java/com/andela/javadevsnairobi/views/DevDetailActivity.java
@@ -1,8 +1,13 @@
 package com.andela.javadevsnairobi.views;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,14 +18,16 @@ import com.squareup.picasso.Picasso;
 
 import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
-public class DevDetailActivity extends AppCompatActivity implements GithubUserView {
+public class DevDetailActivity extends AppCompatActivity implements GithubUserView, View.OnClickListener {
 
     TextView username;
     TextView name;
     TextView githubUrl;
     TextView bio;
     ImageView avatar;
+    Button shareButton;
     GithubPresenter presenter;
+    ConstraintLayout profileView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,10 @@ public class DevDetailActivity extends AppCompatActivity implements GithubUserVi
         githubUrl = findViewById(R.id.dev_github_link);
         bio = findViewById(R.id.dev_bio);
         avatar = findViewById(R.id.dev_avatar);
+        profileView = findViewById(R.id.rectangle);
+
+        shareButton = findViewById(R.id.dev_share_btn);
+        shareButton.setOnClickListener(this);
 
         presenter = new GithubPresenter(this);
         String username = getIntent().getExtras().getString("username");
@@ -59,5 +70,28 @@ public class DevDetailActivity extends AppCompatActivity implements GithubUserVi
         githubUrl.setText(githubUser.getHtmlUrl());
         Picasso.get().load(githubUser.getAvatarUrl())
                 .transform(new CropCircleTransformation()).into(avatar);
+        if (githubUser.getBio() != null)
+            if (githubUser.getBio().contains("\n"))
+                resizeProfileViewHeight(profileView, githubUser.getBio());
+    }
+
+    @Override
+    public void onClick(View v) {
+        String user_name = (String) username.getText();
+        String url = (String) githubUrl.getText();
+        String message = String.format("Checkout this awesome developer @%s, %s", user_name, url);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(Intent.createChooser(shareIntent, "Share using "));
+    }
+
+    public void resizeProfileViewHeight(ConstraintLayout profileView, String bio) {
+        int lines = bio.split("\n").length;
+        int extraHeight = lines * 15;
+        ViewGroup.LayoutParams params = profileView.getLayoutParams();
+        params.height += extraHeight;
+        profileView.setLayoutParams(params);
     }
 }
