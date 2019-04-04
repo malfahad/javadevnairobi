3df7f22657575494fcfde74b82aa708b75f86703
Wasswa Fahad Malende <malende@MacBook-Pro-83.local>
2019-04-04 10:31:53 +0300

    ft(config): handle configuration changes
    - overide onResume and onPause methods
    - serialize model objects with GSON
    
    [FINISHES #156005187]

--- app/src/main/java/com/andela/javadevsnairobi/views/MainActivity.java
+++ app/src/main/java/com/andela/javadevsnairobi/views/MainActivity.java
@@ -1,6 +1,7 @@
 package com.andela.javadevsnairobi.views;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +17,10 @@ import com.andela.javadevsnairobi.R;
 import com.andela.javadevsnairobi.adapter.GithubAdapter;
 import com.andela.javadevsnairobi.model.GithubUser;
 import com.andela.javadevsnairobi.presenter.GithubPresenter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GithubAllUsersView {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements GithubAllUsersVie
     GithubPresenter presenter;
     SwipeRefreshLayout swipeRefreshLayout;
     ProgressDialog progressDialog;
+    List<GithubUser> mGithubUsers;
 
 
     @Override
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements GithubAllUsersVie
         setContentView(R.layout.activity_main);
 
         devsRecyclerView = findViewById(R.id.devs_recycler_view);
-        devsRecyclerView.setLayoutManager(new GridLayoutManager(this,getResources().getInteger(R.integer.column_count)));
+        devsRecyclerView.setLayoutManager(new GridLayoutManager(this, getResources().getInteger(R.integer.column_count)));
         presenter = new GithubPresenter(this);
         presenter.getAllUsers();
 
@@ -52,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements GithubAllUsersVie
 
     @Override
     public void showAllUsers(List<GithubUser> githubUsers) {
+        mGithubUsers = githubUsers;
         GithubAdapter adapter = new GithubAdapter(githubUsers);
         devsRecyclerView.setAdapter(adapter);
         swipeRefreshLayout.setRefreshing(false);
@@ -61,19 +67,42 @@ public class MainActivity extends AppCompatActivity implements GithubAllUsersVie
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.refresh_item:
                 swipeRefreshLayout.setRefreshing(true);
                 presenter.getAllUsers();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Type listType = new TypeToken<List<GithubUser>>() {
+        }.getType();
+        String serailizedGithubUsers = new Gson().toJson(mGithubUsers, listType);
+        SharedPreferences githubUsers = getSharedPreferences("GithubUsers", MODE_PRIVATE);
+        SharedPreferences.Editor editor = githubUsers.edit();
+        editor.putString("github users", serailizedGithubUsers);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Type listType = new TypeToken<List<GithubUser>>() {
+        }.getType();
+        SharedPreferences githubUsers = getSharedPreferences("GithubUsers", MODE_PRIVATE);
+        String serializedGithubUsers = githubUsers.getString("github users", null);
+        List<GithubUser> githubUserList = new Gson().fromJson(serializedGithubUsers, listType);
+        if (githubUserList != null) showAllUsers(githubUserList);
     }
 }
